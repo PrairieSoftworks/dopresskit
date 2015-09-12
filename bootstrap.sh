9a87a08e08be4acf948cda70535b8f35809f34cd
Claire Lee <claire.lee@alumni.tufts.edu>
2015-09-12 01:58:51 -0500

    Adding vagrant config for presskit webserver setup

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 php5 libapache2-mod-php5 php5-mcrypt -y
+
+cp /vagrant/apache2.conf /etc/apache2/apache2.conf
+cp /vagrant/presskit.conf /etc/apache2/sites-available/presskit.conf
+
+a2dissite 000-default && sudo a2ensite presskit
+service apache2 restart
